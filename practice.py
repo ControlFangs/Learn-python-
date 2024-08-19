8e76092bb0914d5db655015997b71816b3e31437
ControlFangs <163154411+ControlFangs@users.noreply.github.com>
2024-08-19 01:38:25 -0500

    loops

--- practice.py
+++ practice.py
@@ -1,2 +1,7 @@
 print("this its the start of something amazing Tyler!")
 print("Lets get started")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
